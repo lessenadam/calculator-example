d40fef459e2a39c64a76f7eb179feec2c7d48d8a
Adam Lessen <lessena@gmail.com>
2016-08-31 15:30:03 -0700

    Fill in proper unicode operators

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -6,34 +6,34 @@ const ButtonContainer = ({ handleClick }) => (
         <table>
           <tBody>
             <tr>
-              <InputButton handleClick={handleClick} value="op"/>
-              <InputButton handleClick={handleClick} value="op"/>
-              <InputButton handleClick={handleClick} value="op"/>
-              <InputButton handleClick={handleClick} value="op"/>
+              <InputButton handleClick={handleClick} value="C"/>
+              <InputButton handleClick={handleClick} value="&plusmn;"/>
+              <InputButton handleClick={handleClick} value="%"/>
+              <InputButton handleClick={handleClick} value="&divide;"/>
             </tr>
             <tr>
               <InputButton handleClick={handleClick} value="7" />
               <InputButton handleClick={handleClick} value="8" />
               <InputButton handleClick={handleClick} value="9" />
-              <InputButton handleClick={handleClick} value="op"/>
+              <InputButton handleClick={handleClick} value="&times;"/>
             </tr>
             <tr>
               <InputButton handleClick={handleClick} value="4" />
               <InputButton handleClick={handleClick} value="5" />
               <InputButton handleClick={handleClick} value="6" />
-              <InputButton handleClick={handleClick} value="op"/>
+              <InputButton handleClick={handleClick} value="&minus;"/>
             </tr>
             <tr>
               <InputButton handleClick={handleClick} value="1" />
               <InputButton handleClick={handleClick} value="2" />
               <InputButton handleClick={handleClick} value="3" />
-              <InputButton handleClick={handleClick} value="op"/>
+              <InputButton handleClick={handleClick} value="+"/>
             </tr>
             <tr>
               <InputButton handleClick={handleClick} value="0" styles="no-right-border"/>
               <InputButton handleClick={handleClick} value="" />
               <InputButton handleClick={handleClick} value="." styles="large"/>
-              <InputButton handleClick={handleClick} value="op"/>
+              <InputButton handleClick={handleClick} value="="/>
             </tr>
           </tBody>
         </table>
