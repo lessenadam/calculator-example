9e24ba222d4645e048083fef44287906104fa4cf
Adam Lessen <lessena@gmail.com>
2016-09-01 08:13:10 -0700

    Fix style specificity in table

--- src/components/InputButton.js
+++ src/components/InputButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const InputButton = ({ value, handleClick, styles }) => (
-  <td className={styles} onClick={() => handleClick(value)}>
+  <td id={styles} onClick={() => handleClick(value)}>
     {value}
   </td>
 );
